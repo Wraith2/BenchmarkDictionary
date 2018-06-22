076f0f4509b2ca036f6fd3594052fadb8621878e
Wraith2 <wraith2@gmail.com>
2018-06-22 21:12:36 +0100

    Add project files.

---
+++ BenchmarkDictionary/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+using Generic.DictionaryDev;
+using Wraith.Collections.Generic;
+
+namespace BenchmarkDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			DictionaryDevConcurrentAccessDetectionTests tests = new DictionaryDevConcurrentAccessDetectionTests();
+			tests.DictionaryDevConcurrentAccessDetection_ValueTypeKey(null).GetAwaiter().GetResult();
+
+
+			//DictionaryDev<int, string> dict = new DictionaryDev<int, string>(5, EqualityComparer<int>.Default);
+
+			//dict.Add(1, "one");
+			//dict.Add(2, "two");
+			//dict.Add(3, "three");
+			//dict.Add(4, "four");
+			//dict.Add(5, "five");
+			//dict.Add(6, "six");
+			//dict.Add(7, "seven");
+			//dict.Add(8, "eight");
+
+			//dict.Remove(2);
+			//dict.Remove(3);
+			//dict.Remove(5);
+
+			//dict.RemoveAll(_ => _==2 ||_==3 || _==5);
+
+			//foreach (KeyValuePair<int, string> pair in dict)
+			//{
+			//	Console.WriteLine(pair.Key.ToString());
+			//}
+
+			//BenchmarkRunner.Run<Benchmarks>();
+
+
+			Console.ReadLine();
+		}
+    }
+
+	
+	
+	[CoreJob]
+	[MemoryDiagnoser]
+	[DisassemblyDiagnoser(printAsm: true, printSource: true,recursiveDepth:4)]
+	public class Benchmarks
+	{
+		private int[] keys;
+		private object[] values;
+		private DictionaryDev<int,object> devDict;
+		private DictionaryRef<int,object> refDict;
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			keys=new int[10000];
+			values=new object[keys.Length];
+			for (int index = 0; index<keys.Length; index+=1)
+			{
+				keys[index]=index;
+				values[index]=new object();
+			}
+			devDict=new DictionaryDev<int, object>(keys.Length);
+			refDict=new DictionaryRef<int, object>(keys.Length);
+			//for (int index = 0; index<keys.Length; index+=1)
+			//{
+			//	devDict.Add(keys[index], values[index]);
+			//	refDict.Add(keys[index], values[index]);
+			//}
+		}
+
+		[IterationSetup]
+		private void InstanceSetup()
+		{
+			for (int index = 0; index<keys.Length; index+=1)
+			{
+				devDict.Add(keys[index], values[index]);
+				refDict.Add(keys[index], values[index]);
+			}
+		}
+
+		[IterationCleanup]
+		private void IterationCleanup()
+		{
+			devDict.Clear();
+			refDict.Clear();
+		}
+
+		//[Benchmark]
+		//public int AddDev()
+		//{
+		//	devDict.Clear();
+		//	int count = 0;
+		//	for (int index = 0; index<keys.Length; index+=1)
+		//	{
+		//		devDict.Add(keys[index], values[index]);
+		//		count+=1;
+		//	}
+		//	return count;
+		//}
+
+		//[Benchmark(Baseline = true)]
+		//public int AddRef()
+		//{
+		//	refDict.Clear();
+		//	int count = 0;
+		//	for (int index = 0; index<keys.Length; index+=1)
+		//	{
+		//		refDict.Add(keys[index], values[index]);
+		//		count+=1;
+		//	}
+		//	return count;
+		//}
+		[Benchmark(Baseline = true)]
+		public int RemoveOddRef()
+		{
+			var remove = new List<int>(refDict.Count);
+			foreach (var key in refDict.Keys)
+			{
+				if ((key & 1)==1)
+				{
+					remove.Add(key);
+				}
+			}
+			foreach (var key in remove)
+			{
+				refDict.Remove(key);
+			}
+			return remove.Count;
+		}
+
+		[Benchmark]
+		public int RemoveOddDev()
+		{
+			return devDict.RemoveAll(
+				(KeyValuePair<int,object> pair) => (pair.Key&1)==1 && !object.ReferenceEquals(pair.Value, null)
+			);
+		}
+	}
+}
---
+++ BenchmarkDictionary/tests.cs
@@ -0,0 +1,160 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+using Wraith.Collections.Generic;
+using Xunit;
+
+namespace Generic.DictionaryDev
+{
+	public class DictionaryDevConcurrentAccessDetectionTests
+	{
+		private async Task DictionaryDevConcurrentAccessDetection<TKey, TValue>(DictionaryDev<TKey, TValue> dictionary, bool isValueType, object comparer, Action<DictionaryDev<TKey, TValue>> add, Action<DictionaryDev<TKey, TValue>> get, Action<DictionaryDev<TKey, TValue>> remove, Action<DictionaryDev<TKey, TValue>> removeOutParam, Action<DictionaryDev<TKey, TValue>> removeAll)
+		{
+			Task task = Task.Factory.StartNew(() =>
+			{
+				// Get the DictionaryDev into a corrupted state, as if it had been corrupted by concurrent access.
+				// We this deterministically by clearing the _entries array using reflection;
+				// this means that every Entry struct has a 'next' field of zero, which causes the infinite loop
+				// that we want DictionaryDev to break out of
+				FieldInfo entriesType = dictionary.GetType().GetField("_entries", BindingFlags.NonPublic | BindingFlags.Instance);
+				Array entriesInstance = (Array)entriesType.GetValue(dictionary);
+				//Array entryArray = (Array)Activator.CreateInstance(entriesInstance.GetType(), new object[] { entriesInstance.Length });
+				//entriesType.SetValue(dictionary, entryArray);
+
+				var entryType = entriesType.GetValue(dictionary).GetType().GetElementType();
+				var nextField = entryType.GetField("next");
+				int firstNext = 0;
+				for (int index = 0; index<entriesInstance.Length; index+=1)
+				{
+					//int next = (int)nextField.GetValue(entriesInstance.GetValue(index));
+					//if (next!=0)
+					//{
+					//	if (firstNext==0)
+					//	{
+					//		firstNext=next;
+					//	}
+					//	else
+					//	{
+					//		nextField.SetValue(entriesInstance.GetValue(index), firstNext);
+					//	}
+					//}
+					nextField.SetValue(entriesInstance.GetValue(index), 0);
+				}
+
+				//Assert.Equal(comparer, dictionary.GetType().GetField("_comparer", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(dictionary));
+				//Assert.Equal(isValueType, dictionary.GetType().GetGenericArguments()[0].IsValueType);
+				//Assert.Equal("ThrowInvalidOperationException_ConcurrentOperationsNotSupported", Assert.Throws<InvalidOperationException>(() => add(dictionary)).TargetSite.Name);
+				//Assert.Equal("ThrowInvalidOperationException_ConcurrentOperationsNotSupported", Assert.Throws<InvalidOperationException>(() => get(dictionary)).TargetSite.Name);
+				//Assert.Equal("ThrowInvalidOperationException_ConcurrentOperationsNotSupported", Assert.Throws<InvalidOperationException>(() => remove(dictionary)).TargetSite.Name);
+				//Assert.Equal("ThrowInvalidOperationException_ConcurrentOperationsNotSupported", Assert.Throws<InvalidOperationException>(() => removeOutParam(dictionary)).TargetSite.Name);
+
+				Assert.Equal("ThrowInvalidOperationException_ConcurrentOperationsNotSupported", Assert.Throws<InvalidOperationException>(() => removeAll(dictionary)).TargetSite.Name);
+			}, TaskCreationOptions.LongRunning);
+
+			// If DictionaryDev regresses, we do not want to hang here indefinitely
+			Assert.True((await Task.WhenAny(task, Task.Delay(TimeSpan.FromSeconds(60))) == task) && task.IsCompletedSuccessfully);
+		}
+
+		//[Theory]
+		//[InlineData(null)]
+		//[InlineData(typeof(CustomEqualityComparerInt32ValueType))]
+		public async Task DictionaryDevConcurrentAccessDetection_ValueTypeKey(Type comparerType)
+		{
+			IEqualityComparer<int> customComparer = null;
+
+			DictionaryDev<int, int> dic = comparerType == null ?
+				new DictionaryDev<int, int>() :
+				new DictionaryDev<int, int>((customComparer = (IEqualityComparer<int>)Activator.CreateInstance(comparerType)));
+
+			dic.Add(1, 1);
+			dic.Add(4, 2);
+
+			await DictionaryDevConcurrentAccessDetection(dic,
+				typeof(int).IsValueType,
+				customComparer,
+				d => d.Add(1, 1),
+				d => { var v = d[1]; },
+				d => d.Remove(1),
+				d => d.Remove(1, out int value),
+				d => d.RemoveAll(kvp => true)
+			);
+		}
+
+		//[Theory]
+		//[InlineData(null)]
+		//[InlineData(typeof(CustomEqualityComparerDummyRefType))]
+		public async Task DictionaryDevConcurrentAccessDetection_ReferenceTypeKey(Type comparerType)
+		{
+			IEqualityComparer<DummyRefType> customComparer = null;
+
+			DictionaryDev<DummyRefType, DummyRefType> dic = comparerType == null ?
+				new DictionaryDev<DummyRefType, DummyRefType>() :
+				new DictionaryDev<DummyRefType, DummyRefType>((customComparer = (IEqualityComparer<DummyRefType>)Activator.CreateInstance(comparerType)));
+
+			var keyValueSample = new DummyRefType() { Value = 1 };
+
+			dic.Add(keyValueSample, keyValueSample);
+
+			await DictionaryDevConcurrentAccessDetection(dic,
+				typeof(DummyRefType).IsValueType,
+				customComparer,
+				d => d.Add(keyValueSample, keyValueSample),
+				d => { var v = d[keyValueSample]; },
+				d => d.Remove(keyValueSample),
+				d => d.Remove(keyValueSample, out DummyRefType value),
+				d => d.RemoveAll(kvp => true)
+			);
+		}
+	}
+
+	// We use a custom type instead of string because string use optimized comparer https://github.com/dotnet/coreclr/blob/master/src/System.Private.CoreLib/shared/System/Collections/Generic/DictionaryDev.cs#L79
+	// We want to test case with _comparer = null
+	[DebuggerStepThrough]
+	class DummyRefType
+	{
+		public int Value { get; set; }
+		public override bool Equals(object obj)
+		{
+			return ((DummyRefType)obj).Equals(this.Value);
+		}
+
+		public override int GetHashCode()
+		{
+			return Value.GetHashCode();
+		}
+	}
+	[DebuggerStepThrough]
+	class CustomEqualityComparerDummyRefType : EqualityComparer<DummyRefType>
+	{
+		public override bool Equals(DummyRefType x, DummyRefType y)
+		{
+			return x.Value == y.Value;
+		}
+
+		public override int GetHashCode(DummyRefType obj)
+		{
+			return obj.GetHashCode();
+		}
+	}
+
+	[DebuggerStepThrough]
+	class CustomEqualityComparerInt32ValueType : EqualityComparer<int>
+	{
+		public override bool Equals(int x, int y)
+		{
+			return EqualityComparer<int>.Default.Equals(x, y);
+		}
+
+		public override int GetHashCode(int obj)
+		{
+			return EqualityComparer<int>.Default.GetHashCode(obj);
+		}
+	}
+}
